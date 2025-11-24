f18d0fe0ba7ee1685174645ca985276ee104ff67
Husni Mubarok <husniiimubarok00@gmail.com>
2025-11-24 22:37:06 +0700

    Add marketing section, components folder, and public assets

---
+++ app/(marketing)/layout.tsx
@@ -0,0 +1,19 @@
+import { Navbar } from "./_components/navbar";
+
+const MarketingLayout = ({
+  children
+}: {
+  children: React.ReactNode;
+}) => {
+  return (
+    <div className="h-[100vh] bg-slate-100">
+      <Navbar/>
+      <main className="pt-40 pb-20 bg-slate-100">
+        {children}
+      </main>
+      {/* {Footer} */}
+    </div>
+  );
+};
+
+export default MarketingLayout;

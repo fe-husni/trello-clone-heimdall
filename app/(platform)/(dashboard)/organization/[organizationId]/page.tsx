6a078f8327e9a9e0f3075e0227d9d57f03eab826
Husni Mubarok <husniiimubarok00@gmail.com>
2025-12-15 16:26:22 +0700

    mau commit ajah✨

--- app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -3,15 +3,34 @@
 import { BoardList } from "./_components/board-list";
 import { Info } from "./_components/info";
 import { Separator } from "@/components/ui/separator";
+import { create } from "@/actions/create-board";
+
+// export const runtime = "nodejs";
+
 
 const OrganizationIdPage = () => {
+  
+  
+
   return (
-    <div className="flex flex-col space-y-4 w-full">
-      <Info/>
-      <Separator/>
-      <div className="px-2 md:px-4">
-        <BoardList/>
-      </div>
+    // <div className="flex flex-col space-y-4 w-full">
+    //   <Info />
+    //   <Separator />
+    //   <div className="px-2 md:px-4">
+    //     <BoardList />
+    //   </div>
+    // </div>
+
+    <div>
+      <form action={create}>
+        <input
+          name="title"
+          id="title"
+          required
+          placeholder="Enter a board title"
+          className="border-black border p-1"
+        />
+      </form>
     </div>
   );
 };

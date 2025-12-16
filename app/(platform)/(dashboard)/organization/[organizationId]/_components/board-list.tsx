b0e9b7b5b3ae1f3f73d1e2d929250e23d6386483
Husni Mubarok <husniiimubarok00@gmail.com>
2025-12-16 20:31:01 +0700

    feat(board): add safe create/delete actions and form components

--- app/(platform)/(dashboard)/organization/[organizationId]/_components/board-list.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/_components/board-list.tsx
@@ -1,3 +1,4 @@
+import { FormPopover } from "@/components/form/form-popover";
 import { Hint } from "@/components/hint"
 import { HelpCircle, SquarePlus, User2 } from "lucide-react"
 
@@ -9,28 +10,21 @@ export const BoardList = () => {
         Your Boards
       </div>
       <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-4">
-        <div role="button" className="aspect-video bg-gray-800 rounded-xl p-5">
-          <h3 className="text-white font-semibold">Board xx</h3>
-        </div>
-        <div role="button" className="aspect-video bg-gray-800 rounded-xl p-5">
-          <h3 className="text-white font-semibold">Board xx</h3>
-        </div>
-        <div role="button" className="aspect-video bg-gray-800 rounded-xl p-5">
-          <h3 className="text-white font-semibold">Board xx</h3>
-        </div>
-        <div role="button" className="aspect-video relative h-full w-full bg-muted rounded-xl flex flex-col gap-y-1 items-center justify-center hover:opacity-75 transition">
-          <SquarePlus />
-          <p className="text-sm">Create new board</p>
-          <span className="text-xs">5 remaining</span>
-          <Hint
-            sideOffset={40}
-            description={`
+        <FormPopover side="right" sideOffset={10}>
+          <div role="button" className="aspect-video relative h-full w-full bg-muted rounded-xl flex flex-col gap-y-1 items-center justify-center hover:opacity-75 transition">
+            <SquarePlus />
+            <p className="text-sm">Create new board</p>
+            <span className="text-xs">5 remaining</span>
+            <Hint
+              sideOffset={40}
+              description={`
               Free Workspaces can have up to 5 open boards. For unlimited boards upgrade this workspace.
-            `}
-          >
-            <HelpCircle className="absolute bottom-2 right-2 h-[14px] w-[14px]" />
-          </Hint>
-        </div>
+              `}
+            >
+              <HelpCircle className="absolute bottom-2 right-2 h-[14px] w-[14px]" />
+            </Hint>
+          </div>
+        </FormPopover>
 
       </div>
     </div>
--- app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -1,36 +1,22 @@
-// import { OrganizationSwitcher } from "@clerk/nextjs";
-// import { auth } from "@clerk/nextjs/server";
-import { BoardList } from "./_components/board-list";
+import { db } from "@/lib/db";
 import { Info } from "./_components/info";
 import { Separator } from "@/components/ui/separator";
-import { create } from "@/actions/create-board";
+import { BoardList } from "./_components/board-list";
 
 // export const runtime = "nodejs";
 
 
-const OrganizationIdPage = () => {
-  
+const OrganizationIdPage = async () => {
+  const boards = await db.board.findMany()
   
 
   return (
-    // <div className="flex flex-col space-y-4 w-full">
-    //   <Info />
-    //   <Separator />
-    //   <div className="px-2 md:px-4">
-    //     <BoardList />
-    //   </div>
-    // </div>
-
-    <div>
-      <form action={create}>
-        <input
-          name="title"
-          id="title"
-          required
-          placeholder="Enter a board title"
-          className="border-black border p-1"
-        />
-      </form>
+    <div className="flex flex-col space-y-4 w-full">
+      <Info />
+      <Separator />
+      <div className="px-2 md:px-4">
+        <BoardList />
+      </div>
     </div>
   );
 };

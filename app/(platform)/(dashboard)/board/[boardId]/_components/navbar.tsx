ef5fbf4e7c67b01e583f56504311e5fae0cdce7c
Husni Mubarok <husniiimubarok00@gmail.com>
2025-12-24 16:56:14 +0700

    maw commit ajah✨

--- app/(platform)/(dashboard)/board/[boardId]/_components/navbar.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/navbar.tsx
@@ -12,7 +12,7 @@ export const BoardNavbar = async ({
 
   return (
     <div className="w-full h-14 z-[40] bg-black/50 fixed top-14 flex items-center px-6 gap-x-4 text-white">
-      Board Navbar!
+      {data.title}
     </div>
   );
 };

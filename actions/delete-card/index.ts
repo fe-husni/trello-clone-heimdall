ed9b9c4bb9cea2ef087a05fd8406ed27cf355b6a
Husni Mubarok <husniiimubarok00@gmail.com>
2026-01-03 20:16:53 +0700

    feat(card): implement editable description and card actions✨
    - Add editable card description with textarea form
    - Handle save & cancel state for description editing
    - Add copy card action
    - Add delete card action
    - Update card modal UI and related server actions

---
+++ actions/delete-card/index.ts
@@ -0,0 +1,45 @@
+"use server";
+
+import { auth } from "@clerk/nextjs/server";
+import { InputType } from "./types";
+import { db } from "@/lib/db";
+import { revalidatePath } from "next/cache";
+import { createSafeAction } from "@/lib/create-safe-action";
+import { DeleteCard } from "./schema";
+import { redirect } from "next/navigation";
+import { orderBy } from "lodash";
+import { error } from "console";
+
+const handler = async (data: InputType) => {
+  const { userId, orgId } = await auth();
+
+  if (!userId || !orgId) {
+    return { error: "Unauthorized" };
+  }
+
+  const { id, boardId } = data;
+  let card;
+
+  try {
+    card = await db.card.delete({
+      where: {
+        id,
+        list: {
+          board: {
+            orgId,
+          },
+        },
+      },
+    });
+
+  } catch (error) {
+    console.error(error);
+    return { error: "Failed to delete." };
+  }
+
+  revalidatePath(`/board/${boardId}`);
+  return { data: card };
+};
+
+
+export const deleteCard = createSafeAction(DeleteCard, handler);
--- components/modals/card-modal/index.tsx
+++ components/modals/card-modal/index.tsx
@@ -6,13 +6,15 @@ import { useCardModal } from "@/hooks/use-card-modal";
 import { CardWithList } from "@/types";
 import { fetcher } from "@/lib/fetcher";
 import { Header } from "./header";
+import { Description } from "./description";
+import { Actions } from "./actions";
 
 export const CardModal = () => {
   const id = useCardModal((state) => state.id);
   const isOpen = useCardModal((state) => state.isOpen);
   const onClose = useCardModal((state) => state.onClose);
 
-  const {data: cardData} = useQuery<CardWithList>({
+  const { data: cardData } = useQuery<CardWithList>({
     queryKey: ["card", id],
     queryFn: () => fetcher(`/api/cards/${id}`),
   });
@@ -24,9 +26,24 @@ export const CardModal = () => {
     >
       <DialogContent>
         {!cardData
-          ? <Header.Skeleton/>
-          : <Header data={cardData}/>
+          ? <Header.Skeleton />
+          : <Header data={cardData} />
         }
+
+        <div className="grid grid-cols-1 md:grid-cols-4 md:gap-4">
+          <div className="col-span-3">
+            <div className="w-full space-y-6">
+              {!cardData
+                ? <Description.Skeleton />
+                : <Description data={cardData} />
+              }
+            </div>
+          </div>
+          {!cardData
+            ? <Actions.Skeleton/>
+            : <Actions data={cardData}/>
+          }
+        </div>
       </DialogContent>
     </Dialog>
   )
